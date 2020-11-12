41046d132e93791610b93449d5a26a2e244b6e94
Andrea Veneziano <andrea.veneziano@epfl.ch>
2020-11-12 17:01:07 +0100

    Add initial files

---
+++ build.gradle.kts
@@ -0,0 +1,73 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.gradle.api.tasks.testing.logging.TestLogEvent.*
+  import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  kotlin ("jvm") version "1.3.72"
+  application
+  id("com.github.johnrengelman.shadow") version "5.2.0"
+}
+
+group = "ch.biot"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+  mavenCentral()
+  jcenter()
+}
+
+val kotlinVersion = "1.3.72"
+val vertxVersion = "4.0.0.CR1"
+val junitJupiterVersion = "5.6.0"
+
+val mainVerticleName = "ch.biot.backend.MainVerticle"
+val watchForChange = "src/**/*"
+val doOnChange = "./gradlew classes"
+val launcherClassName = "io.vertx.core.Launcher"
+
+application {
+  mainClassName = launcherClassName
+}
+
+dependencies {
+  implementation("io.vertx:vertx-web-client:$vertxVersion")
+  implementation("io.vertx:vertx-auth-jwt:$vertxVersion")
+  implementation("io.vertx:vertx-health-check:$vertxVersion")
+  implementation("io.vertx:vertx-web:$vertxVersion")
+  implementation("io.vertx:vertx-auth-mongo:$vertxVersion")
+  implementation("io.vertx:vertx-hazelcast:$vertxVersion")
+  implementation("io.vertx:vertx-micrometer-metrics:$vertxVersion")
+  implementation("io.vertx:vertx-sql-client-templates:$vertxVersion")
+  implementation("io.vertx:vertx-pg-client:$vertxVersion")
+  implementation("io.vertx:vertx-lang-kotlin-coroutines:$vertxVersion")
+  implementation("io.vertx:vertx-kafka-client:$vertxVersion")
+  implementation("io.vertx:vertx-mongo-client:$vertxVersion")
+  implementation("io.vertx:vertx-lang-kotlin:$vertxVersion")
+  implementation(kotlin("stdlib-jdk8"))
+  testImplementation("io.vertx:vertx-junit5:$vertxVersion")
+  testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+}
+
+  val compileKotlin: KotlinCompile by tasks
+  compileKotlin.kotlinOptions.jvmTarget = "11"
+
+tasks.withType<ShadowJar> {
+  archiveClassifier.set("fat")
+  manifest {
+    attributes(mapOf("Main-Verticle" to mainVerticleName))
+  }
+  mergeServiceFiles {
+    include("META-INF/services/io.vertx.core.spi.VerticleFactory")
+  }
+}
+
+tasks.withType<Test> {
+  useJUnitPlatform()
+  testLogging {
+    events = setOf(PASSED, SKIPPED, FAILED)
+  }
+}
+
+tasks.withType<JavaExec> {
+  args = listOf("run", mainVerticleName, "--redeploy=$watchForChange", "--launcher-class=$launcherClassName", "--on-redeploy=$doOnChange")
+}
