85293295d38b25db0b3fd6b0f4c641ba0402cf75
Andrea Veneziano <andrea.veneziano@epfl.ch>
2020-11-21 23:31:05 +0100

    Add dependencies for tests

--- ingestion/build.gradle.kts
+++ ingestion/build.gradle.kts
@@ -1,9 +1,9 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.gradle.api.tasks.testing.logging.TestLogEvent.*
-  import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  kotlin ("jvm") version "1.3.72"
+  kotlin("jvm") version "1.3.72"
   application
   id("com.github.johnrengelman.shadow") version "5.2.0"
 }
@@ -19,6 +19,7 @@ repositories {
 val kotlinVersion = "1.3.72"
 val vertxVersion = "4.0.0.CR1"
 val junitJupiterVersion = "5.6.0"
+val restAssuredVersion = "4.3.2"
 
 val mainVerticleName = "ch.biot.backend.ingestion.IngestionVerticle"
 val watchForChange = "src/**/*"
@@ -39,12 +40,16 @@ dependencies {
   implementation("io.vertx:vertx-mqtt:$vertxVersion")
   implementation("ch.qos.logback:logback-classic:1.2.3")
   implementation(kotlin("stdlib-jdk8"))
+  testImplementation("io.rest-assured:rest-assured:$restAssuredVersion")
+  testImplementation("io.rest-assured:kotlin-extensions:$restAssuredVersion")
   testImplementation("io.vertx:vertx-junit5:$vertxVersion")
+  testImplementation("io.vertx:vertx-junit5-rx-java2:$vertxVersion")
   testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+  testImplementation("org.testcontainers:testcontainers:1.15.0")
 }
 
-  val compileKotlin: KotlinCompile by tasks
-  compileKotlin.kotlinOptions.jvmTarget = "11"
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions.jvmTarget = "11"
 
 tasks.withType<ShadowJar> {
   archiveClassifier.set("fat")
@@ -64,6 +69,12 @@ tasks.withType<Test> {
 }
 
 tasks.withType<JavaExec> {
-  args = listOf("run", mainVerticleName, "--redeploy=$watchForChange", "--launcher-class=$launcherClassName", "--on-redeploy=$doOnChange")
+  args = listOf(
+    "run",
+    mainVerticleName,
+    "--redeploy=$watchForChange",
+    "--launcher-class=$launcherClassName",
+    "--on-redeploy=$doOnChange"
+  )
   systemProperties["vertx.logger-delegate-factory-class-name"] = "io.vertx.core.logging.SLF4JLogDelegateFactory"
 }
