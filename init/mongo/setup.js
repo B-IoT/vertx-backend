867ea6e0a4a9a5dfa4f408b7d26029eb9805eece
Andrea Veneziano <andrea.veneziano@epfl.ch>
2020-11-25 17:36:11 +0100

    Update Mongo setup for authenticating relays

--- init/mongo/setup.js
+++ init/mongo/setup.js
@@ -1,3 +1,5 @@
 db.createCollection("relays");
 
 db.relays.createIndex({relayID: 1}, {unique: true});
+db.relays.createIndex({mqttID: 1}, {unique: true});
+db.relays.createIndex({mqttUsername: 1}, {unique: true});
