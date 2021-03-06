9651ac39f3a35cecbd8682c2857a01e26b63d92c
Andrea Veneziano <andrea.veneziano@epfl.ch>
2021-03-07 00:15:13 +0100

    Add utility shell scripts for deployment and certs renewal

---
+++ deploy.sh
@@ -0,0 +1,1 @@
+skaffold run -d eu.gcr.io/dark-mark-304414
---
+++ renew-certs.sh
@@ -0,0 +1,1 @@
+acme.sh --renew -d b-iot.ch -d api.b-iot.ch -d mqtt.b-iot.ch -d pgadmin.b-iot.ch --yes-I-know-dns-manual-mode-enough-go-ahead-please
