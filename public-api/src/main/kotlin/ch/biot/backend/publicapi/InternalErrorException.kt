5a432abeecab8be93cf3fec0716f139ea296d663
Samuel Chassot <14821693+samuelchassot@users.noreply.github.com>
2021-08-09 14:44:00 +0200

    BUG: relays `sendLastConfig` was always looking in the same table of the DB i.e. `relays` and not `relays_<company>` (#106)
    * correect bug
    
    * begin to add tests DOES NOT WORK
    
    * tests pass
    
    * add a test that triggers the bug
    
    * formatting

--- public-api/src/main/kotlin/ch/biot/backend/publicapi/InternalErrorException.kt
+++ public-api/src/main/kotlin/ch/biot/backend/publicapi/InternalErrorException.kt
@@ -7,4 +7,4 @@ package ch.biot.backend.publicapi
 /**
  * Exception modeling an internal error (HTTP code 500)
  */
-class InternalErrorException(message: String, cause: Throwable?): Exception(message, cause)
+class InternalErrorException(message: String, cause: Throwable?) : Exception(message, cause)
